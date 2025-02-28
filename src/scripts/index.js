9c6a8a10cf36d8fb9fe5b2d44d8a473466732ce0
GKVS <itsergey1001@gmail.com>
2025-02-28 08:47:04 +0500

    feat: adaptive single page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -42,4 +42,79 @@ document.addEventListener('DOMContentLoaded', () => {
 	collapseHeaderButton.addEventListener('click', function() {
 		collapseHeaderMenus.forEach(menu => menu.toggle());
 	})
+
+	const thumbnailSlider = document.querySelector('.thumbnail-slider');
+	const childrenThumbnailSlider = [...thumbnailSlider.children].reverse();
+
+	// Устанавливаем класс для последнего видимого элемента в слайдере
+	for (const child of childrenThumbnailSlider) {
+		if(child.getBoundingClientRect().left < thumbnailSlider.getBoundingClientRect().right) {
+			child.classList.add('thumbnail-slider__last')
+			break;
+		}
+	}
+
+	// Устанавливаем css переменные для слайдера
+	const containerWidth = document.querySelector('.thumbnail-slider');
+
+	containerWidth.style.setProperty('--container-width', containerWidth.offsetWidth);
+
+
+
+
+	
 });
+
+
+const verticalMenu = document.querySelector('.list-group-widget');
+const backgroundItem = verticalMenu.querySelector('.background-item');
+let activeMenuItem = verticalMenu.querySelector('.active') || false;
+
+function setBackgroundItem(activeItem) {
+	if(!activeMenuItem) return;
+
+	const top = activeItem.offsetTop;
+	const left = activeItem.offsetLeft;
+	const width = activeItem.offsetWidth;
+	const height = activeItem.offsetHeight;
+
+	backgroundItem.style.setProperty('--top-active', top + 'px'); 
+	backgroundItem.style.setProperty('--left-active', left + 'px');
+	backgroundItem.style.setProperty('--width-active', width + 'px');
+	backgroundItem.style.setProperty('--height-active', height + 'px');
+}
+
+function handleMenuItemClick(e) {
+
+	const target = e.target;
+	if(target.tagName !== 'A') return;
+
+	activeMenuItem.classList.remove('active');
+	activeMenuItem = target;
+	activeMenuItem.classList.add('active');
+
+	setBackgroundItem(activeMenuItem);
+}
+
+
+function handleMenuItemIn(e) {
+	const target = e.target;
+	if(target.tagName !== 'A') return;
+
+	activeMenuItem.classList.remove('active');
+	setBackgroundItem(target);
+}
+
+function handleMenuItemOut(e) {
+	const target = e.target;
+	if(target.tagName !== 'A') return;
+
+	activeMenuItem.classList.add('active');
+	setBackgroundItem(activeMenuItem);
+}
+
+setBackgroundItem(activeMenuItem);
+verticalMenu.addEventListener('click', handleMenuItemClick);
+verticalMenu.addEventListener('mouseover', handleMenuItemIn);
+verticalMenu.addEventListener('mouseout', handleMenuItemOut);
+
