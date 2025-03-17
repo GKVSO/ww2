e2cb52d0c7d4a32f53ebe57b98cbe3f7f8294fa9
GKVS <itsergey1001@gmail.com>
2025-03-17 14:33:46 +0500

    fix: commission page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -113,10 +113,16 @@ document.addEventListener('DOMContentLoaded', () => {
 		console.error('Error initializing fancyWebp:', error);
 	}
 
-	const openGallery = document.querySelector('#openGallery')
-	openGallery.addEventListener('click', function() {
-		Fancybox.fromSelector('[data-fancybox="gallery"]');
-	})
+
+	try {
+		const openGallery = document.querySelector('#openGallery')
+		openGallery.addEventListener('click', function() {
+			Fancybox.fromSelector('[data-fancybox="gallery"]');
+		})
+	} catch(error) {
+		console.error('Error initializing open gallery btn:', error);
+	}
+	
 	
 
 	// Collapse footer menus
@@ -187,14 +193,202 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Создание карты
 	const map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 17, // Уровень масштабирования
-		center: myLatLng // Центр карты
+		center: myLatLng, // Центр карты
+		styles: [
+			{
+					"featureType": "water",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#e9e9e9"
+							},
+							{
+									"lightness": 17
+							}
+					]
+			},
+			{
+					"featureType": "landscape",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#f5f5f5"
+							},
+							{
+									"lightness": 20
+							}
+					]
+			},
+			{
+					"featureType": "road.highway",
+					"elementType": "geometry.fill",
+					"stylers": [
+							{
+									"color": "#ffffff"
+							},
+							{
+									"lightness": 17
+							}
+					]
+			},
+			{
+					"featureType": "road.highway",
+					"elementType": "geometry.stroke",
+					"stylers": [
+							{
+									"color": "#ffffff"
+							},
+							{
+									"lightness": 29
+							},
+							{
+									"weight": 0.2
+							}
+					]
+			},
+			{
+					"featureType": "road.arterial",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#ffffff"
+							},
+							{
+									"lightness": 18
+							}
+					]
+			},
+			{
+					"featureType": "road.local",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#ffffff"
+							},
+							{
+									"lightness": 16
+							}
+					]
+			},
+			{
+					"featureType": "poi",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#f5f5f5"
+							},
+							{
+									"lightness": 21
+							}
+					]
+			},
+			{
+					"featureType": "poi.park",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#dedede"
+							},
+							{
+									"lightness": 21
+							}
+					]
+			},
+			{
+					"elementType": "labels.text.stroke",
+					"stylers": [
+							{
+									"visibility": "on"
+							},
+							{
+									"color": "#ffffff"
+							},
+							{
+									"lightness": 16
+							}
+					]
+			},
+			{
+					"elementType": "labels.text.fill",
+					"stylers": [
+							{
+									"saturation": 36
+							},
+							{
+									"color": "#333333"
+							},
+							{
+									"lightness": 40
+							}
+					]
+			},
+			{
+					"elementType": "labels.icon",
+					"stylers": [
+							{
+									"visibility": "off"
+							}
+					]
+			},
+			{
+					"featureType": "transit",
+					"elementType": "geometry",
+					"stylers": [
+							{
+									"color": "#f2f2f2"
+							},
+							{
+									"lightness": 19
+							}
+					]
+			},
+			{
+					"featureType": "administrative",
+					"elementType": "geometry.fill",
+					"stylers": [
+							{
+									"color": "#fefefe"
+							},
+							{
+									"lightness": 20
+							}
+					]
+			},
+			{
+					"featureType": "administrative",
+					"elementType": "geometry.stroke",
+					"stylers": [
+							{
+									"color": "#fefefe"
+							},
+							{
+									"lightness": 17
+							},
+							{
+									"weight": 1.2
+							}
+					]
+			}
+	]
 	});
 
 	// Добавление маркера
 	const marker = new google.maps.Marker({
 		position: myLatLng,
 		map: map,
-		title: 'Привет, это Москва!'
+    clickable: true, // Делаем кликабельным (по умолчанию true)
+		icon: {
+			url: '../assets/images/google-mark1.png',
+			scaledSize: new google.maps.Size(30, 30)
+		}
 	});
-	
+	// Инфо-окно
+	const infoWindow = new google.maps.InfoWindow({
+		content: "Ленинградское шоссе 37 корп.1, г.Москва"
+	});
+
+	// Обработчик клика
+	marker.addListener('click', () => {
+		infoWindow.open(map, marker);
+	});
 });
